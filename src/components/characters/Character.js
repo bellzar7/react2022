caa75b3cfc04b47c05003e11bde448c064d97c64
bellzar7 <belelya.nazar1999@gmail.com>
2022-06-21 18:59:46 +0300

    react1lesson

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -5,6 +5,7 @@ let {name, desc, picture} = props
         <div className={'divFromApp'}>
             <h2>{name}</h2>
             <p>{desc}</p>
+
             <img src={picture} alt=""/>
         </div>
     )
