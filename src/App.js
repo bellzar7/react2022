4dab8e7bd0915ae488474cd83a73cd27b22b5ec5
bellzar7 <belelya.nazar1999@gmail.com>
2022-06-21 19:24:16 +0300

    react cw1

--- src/App.js
+++ src/App.js
@@ -8,13 +8,11 @@ function App() {
       name={'BART'}
       desc={'lorem10'}
       picture={'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'}
-
       />
         <Character
         name={'Homer'}
         desc={'asdqwe qwe dsa cvbsg'}
         picture={'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}
-
         />
     </div>
   );
