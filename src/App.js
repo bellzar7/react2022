93fe22d9f5de6a4794c54d319eca7ed352ab251d
bellzar7 <belelya.nazar1999@gmail.com>
2022-06-21 19:43:34 +0300

    обновив мастерс

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import Character from "./components/characters/Character";
+
 
 function App() {
   return (
-    <div  >
-      <Character
-      name={'BART'}
-      desc={'lorem10'}
-      picture={'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'}
-      />
-        <Character
-        name={'Homer'}
-        desc={'asdqwe qwe dsa cvbsg'}
-        picture={'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}
-        />
+    <div >
+
     </div>
   );
 }
