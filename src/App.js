f565bd11fc89be6a31b4a7839cb3826ec46c0d4a
bellzar7 <belelya.nazar1999@gmail.com>
2022-06-21 22:19:42 +0300

    hw1 task 1 - done

--- src/App.js
+++ src/App.js
@@ -14,6 +14,16 @@ function App() {
         desc={'asdqwe qwe dsa cvbsg'}
         picture={'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}
         />
+        <Character
+            name={'Marge'}
+            desc={'asdqwe qwe dsa cvbsg'}
+            picture={'https://upload.wikimedia.org/wikipedia/uk/0/0b/Marge_Simpson.png'}
+        />
+        <Character
+            name={'Lisa'}
+            desc={'asdqwe qwe dsa cvbsg'}
+            picture={'https://i.pinimg.com/originals/94/c8/ba/94c8ba735b7846c5e321ff299d0a425a.jpg'}
+        />
     </div>
   );
 }
