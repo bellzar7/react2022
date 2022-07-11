ea3aae914a70767e3d69d18ea5829db358953af4
bellzar7 <belelya.nazar1999@gmail.com>
2022-07-11 23:40:45 +0300

    hw 5 routes pt2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Users from "./components/Users/Users";
+import {Route,Routes} from "react-router-dom";
 
 
 function App() {
     return (
         <div>
+            <Routes>
+                <Route/>
+            </Routes>
             <Users/>
 
         </div>
