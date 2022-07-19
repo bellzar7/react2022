f1c6813ae55af2e63d538b779f0c51f988e3876b
bellzar7 <belelya.nazar1999@gmail.com>
2022-07-19 13:11:05 +0300

    class work 7 redux pt1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 
 function App() {
-  return (
-    <div >
 
-    </div>
-  );
+    let state = useSelector(state => state)
+    let dispatch = useDispatch();
+
+    return (
+        <div>
+            {state}
+            <button onClick={()=> {
+                dispatch({type: 'INC', payload: 1})
+            }}>INCrement</button>
+
+            <button onClick={() => {
+                dispatch({type: 'DEC', payload: 1})
+            }}>DECrement
+            </button>
+
+            <button onClick={() => {
+                dispatch({type: 'RES'})
+            }}>RESet
+            </button>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import {createStore} from "redux";
 
+
+let store = createStore((state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state + action.payload;
+        case 'DEC':
+            return state - action.payload;
+        case 'RES':
+            return 0
+
+        default:
+            return state
+    }
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
