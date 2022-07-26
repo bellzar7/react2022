105ccb59b516049e8f9fbcf0fe7ef3ab9d9b3f45
bellzar7 <belelya.nazar1999@gmail.com>
2022-07-26 21:14:04 +0300

    pagination

--- src/pages/CarsPage.js
+++ src/pages/CarsPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CarsPage = () => {
     return (
         <div>
-
+cars
         </div>
     );
 };
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const RegisterPage = () => {
     return (
         <div>
-
+reg
         </div>
     );
 };
